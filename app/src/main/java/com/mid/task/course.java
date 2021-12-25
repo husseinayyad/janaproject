7f68a3f424b8601d306292fbb56dd4a20f05d26b
hussein <husseinayyad70@gmail.com>
2021-12-25 12:40:10 +0200

    new

---
+++ app/src/main/java/com/mid/task/course.java
@@ -0,0 +1,56 @@
+package com.mid.task;
+
+public class course {
+    String android ,java,database,datastr,oop;
+
+    public course() {
+    }
+
+    public course(String android, String java, String database, String datastr, String oop) {
+        this.android = android;
+        this.java = java;
+        this.database = database;
+        this.datastr = datastr;
+        this.oop = oop;
+    }
+
+    public String getAndroid() {
+        return android;
+    }
+
+    public void setAndroid(String android) {
+        this.android = android;
+    }
+
+    public String getJava() {
+        return java;
+    }
+
+    public void setJava(String java) {
+        this.java = java;
+    }
+
+    public String getDatabase() {
+        return database;
+    }
+
+    public void setDatabase(String database) {
+        this.database = database;
+    }
+
+    public String getDatastr() {
+        return datastr;
+    }
+
+    public void setDatastr(String datastr) {
+        this.datastr = datastr;
+    }
+
+    public String getOop() {
+        return oop;
+    }
+
+    public void setOop(String oop) {
+        this.oop = oop;
+    }
+}
